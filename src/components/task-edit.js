629f865c1fc8ddaf6bf855885600496ebb692c00
Рябов <dmitryabov2017@yandex.ru>
2020-04-28 10:35:58 +0300

    19. Учебный проект: большие перемены (Часть 2)

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -168,8 +168,6 @@ export default class TaskEdit extends AbstractSmartComponent {
 
   _applyFlatpickr() {
     if (this._flatpickr) {
-      // При своем создании `flatpickr` дополнительно создает вспомогательные DOM-элементы.
-      // Что бы их удалять, нужно вызывать метод `destroy` у созданного инстанса `flatpickr`.
       this._flatpickr.destroy();
       this._flatpickr = null;
     }
